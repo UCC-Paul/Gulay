43fd4ecf2d18248efb62ff846c7de68ebdd21f8a
Paul <128304338+UCC-Paul@users.noreply.github.com>
2023-12-01 22:25:43 +0800

    separated navbar

--- components/detail-sitao.php
+++ components/detail-sitao.php
@@ -35,37 +35,7 @@
   <body>
 
     <!-- Navbar Start -->
-    <div class="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s">
-        <div class="top-bar row gx-0 align-items-center d-none d-lg-flex">
-            <div class="col-lg-6 px-5 text-start">
-                <small><i class="fa fa-map-marker-alt me-2"></i>University of Caloocan City</small>
-                <small class="ms-4"><i class="fa fa-envelope me-2"></i>vegetalbite@info.org</small>
-            </div>
-            <div class="col-lg-6 px-5 text-end">
-                <small>Follow us:</small>
-                <a class="text-body ms-3" href="https://facebook.com"><i class="fab fa-facebook-f"></i></a>
-
-            </div>
-        </div>
-
-        <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-            <a href="../index.php" class="navbar-brand ms-4 ms-lg-0">
-                <h1 class="fw-bold text-primary m-0">Vegetal<span class="text-secondary">Bi</span>te</h1>
-            </a>
-            <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <div class="navbar-nav ms-auto p-4 p-lg-0">
-                    <a href="../index.php" class="nav-item nav-link">Home</a>
-                    <a href="../gallery.php" class="nav-item nav-link">Gallery</a>
-                    <a href="../calendar.php" class="nav-item nav-link">Crop Calendar</a>
-                    <a href="../about.php" class="nav-item nav-link">About Us</a>
-                    <a href="../ai.php" class="nav-item nav-link">AI Soil Identifier</a>
-                </div>
-            </div>
-        </nav>
-    </div>
+    <?php include 'navbar-detail.php' ?>
     <!-- Navbar End -->
 
     <!-- Page Header Start -->
--- components/navbar-detail.php
+++ components/navbar-detail.php
@@ -12,7 +12,7 @@
         </div>
 
         <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-            <a href="index.php" class="navbar-brand ms-4 ms-lg-0">
+            <a href="../index.php" class="navbar-brand ms-4 ms-lg-0">
                 <h1 class="fw-bold text-primary m-0">Vegetal<span class="text-secondary">Bi</span>te</h1>
             </a>
             <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -20,11 +20,11 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <div class="navbar-nav ms-auto p-4 p-lg-0">
-                    <a href="index.php" class="nav-item nav-link">Home</a>
-                    <a href="gallery.php" class="nav-item nav-link">Gallery</a>
-                    <a href="calendar.php" class="nav-item nav-link">Crop Calendar</a>
-                    <a href="ai.php" class="nav-item nav-link">AI Soil Identifier</a>
-                    <a href="about.php" class="nav-item nav-link">About Us</a>
+                    <a href="../index.php" class="nav-item nav-link">Home</a>
+                    <a href="../gallery.php" class="nav-item nav-link">Gallery</a>
+                    <a href="../calendar.php" class="nav-item nav-link">Crop Calendar</a>
+                    <a href="../ai.php" class="nav-item nav-link">AI Soil Identifier</a>
+                    <a href="../about.php" class="nav-item nav-link">About Us</a>
                 </div>
             </div>
         </nav>
